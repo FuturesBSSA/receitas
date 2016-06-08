3679fc05431b30620b4091a3e3da98bafdbbfb46
Melchior Rutte <melchior.rutte@gmail.com>
2016-06-08 20:28:40 +0200

    edit recipe show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.destroy_all
 Recipe.destroy_all
+User.destroy_all
 
 lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida eleifend erat aliquam pretium. Nulla non felis purus. Cras volutpat pulvinar lectus, efficitur vestibulum mauris cursus sed. Praesent et iaculis sapien, vitae ultricies eros. Ut quis nibh a sem commodo consectetur. Integer accumsan eros mattis augue imperdiet congue. Nam nec sapien condimentum, ullamcorper nulla eu, imperdiet augue. Donec pharetra quam et nulla porta ultricies. Maecenas ornare ex quis tortor sagittis, ut dapibus est vestibulum. Aenean molestie libero et nisl efficitur, in porttitor lacus ultrices. Ut sit amet urna a quam vehicula maximus. Ut id leo vel quam eleifend ullamcorper a eu quam. Aenean a facilisis justo, non fringilla nibh. Pellentesque mollis, lorem ac convallis laoreet, ante dui auctor massa, nec placerat magna velit in mi. Sed consequat, libero sed volutpat blandit, ligula turpis ullamcorper orci, sit amet tincidunt quam sapien a justo.
 
@@ -20,9 +20,9 @@ koen = User.create!(first_name: "Koen", last_name: "k", email: "koen@k.com", pas
 puts "User created!"
 
 3.times do
-Recipe.create!(user: mel, name: "Aspergesoep", summary: "Smaakvolle aspergesoep met een rommig tintje", description: lorem, kitchen: "Nederlands", course: "Voorgerecht", prep_time: 15, photo: File.open(Rails.root.join("db/fixtures/images", "aspergesoep.jpg")))
-Recipe.create!(user: mel, name: "Lasagna Pollo", summary: "Lasange met kip en paprika", description: lorem, kitchen: "Italiaans", course: "Hoofdgerecht", prep_time: 30, photo: File.open(Rails.root.join("db/fixtures/images", "lasagna.jpg")))
-Recipe.create!(user: koen, name: "Chocolade mouse", summary: "Een mouse met chocolade stukjes", description: lorem, kitchen: "Frans", course: "Nagerecht", prep_time: 20, photo: File.open(Rails.root.join("db/fixtures/images", "mouse.jpg")))
+Recipe.create!(user: mel, name: "Aspergesoep", summary: "Smaakvolle aspergesoep met een rommig tintje", description: lorem, kitchen: "Nederlands", course: "Voorgerecht", season: "Lente", number_of_persons: 4, prep_time: 15, photo: File.open(Rails.root.join("db/fixtures/images", "aspergesoep.jpg")))
+Recipe.create!(user: mel, name: "Lasagna Pollo", summary: "Lasange met kip en paprika", description: lorem, kitchen: "Italiaans", course: "Hoofdgerecht", prep_time: 30, season: "Zomer", number_of_persons: 2, photo: File.open(Rails.root.join("db/fixtures/images", "lasagna.jpg")))
+Recipe.create!(user: koen, name: "Chocolade mouse", summary: "Een mouse met chocolade stukjes", description: lorem, kitchen: "Frans", course: "Nagerecht", prep_time: 20, season: "Herfst", number_of_persons: 4, photo: File.open(Rails.root.join("db/fixtures/images", "mouse.jpg")))
 end
 
 puts "Recipes created!"
