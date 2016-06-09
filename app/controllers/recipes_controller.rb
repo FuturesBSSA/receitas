bd157f57dd92668dd79a402e72c3a65f64599a1a
Melchior Rutte <melchior.rutte@gmail.com>
2016-06-09 19:27:23 +0200

    prepare for heroku

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
-  before_action :find_recipe, only: [:favorite, :upvote]
+  before_action :find_recipe, only: [:favorite, :upvote, :index]
 
   def index
     if params[:search]
