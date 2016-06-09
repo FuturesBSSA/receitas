26451d9c699e3cf10ddd987b63166c5430d90fb4
Melchior Rutte <melchior.rutte@gmail.com>
2016-06-09 14:11:10 +0200

    edit domain name for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "receptrnb.herokuapp.com",
+    domain: "zerra.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
